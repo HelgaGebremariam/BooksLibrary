af52558c96586354f3cff74ecdb2875906582ffe
helga <Helga.gebremariam@gmail.com>
2017-04-06 00:54:40 +0300

    Upload dirty variant.

---
+++ XMLSchemaVerification/BooksSchemaVerification.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml;
+using System.Xml.Schema;
+
+namespace XMLSchemaVerification
+{
+    public class BooksSchemaVerification
+    {
+        XmlReaderSettings settings;
+        public BooksSchemaVerification()
+        {
+            settings = new XmlReaderSettings();
+
+            settings.Schemas.Add("http://library.by/catalog", "Books.xsd");
+            settings.ValidationEventHandler +=
+                delegate (object sender, ValidationEventArgs e)
+                {
+                    throw new Exception(String.Format("[{0}:{1}] {2}", e.Exception.LineNumber, e.Exception.LinePosition, e.Message));
+                };
+
+            settings.ValidationFlags = settings.ValidationFlags | XmlSchemaValidationFlags.ReportValidationWarnings;
+            settings.ValidationType = ValidationType.Schema;
+        }
+
+        public void Check()
+        {
+            XmlReader reader = XmlReader.Create("F:\\books.xml", settings);
+           
+            while (reader.Read())
+            {
+            };
+        }
+    }
+}
---
+++ XMLSchemaVirification.Tests/SchemaVirificationTests.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using XMLSchemaVerification;
+
+namespace XMLSchemaVirification.Tests
+{
+    [TestClass]
+    public class SchemaVirificationTests
+    {
+        [TestMethod]
+        public void TestValidSchema_AreCorrectResults()
+        {
+            BooksSchemaVerification sv = new BooksSchemaVerification();
+            sv.Check();
+        }
+    }
+}
