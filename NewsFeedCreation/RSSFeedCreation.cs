e27fd649e0c253d866e90205b8d90c839e813cc1
helga <Helga.gebremariam@gmail.com>
2017-04-07 08:58:33 +0300

    Remove dll, pdb

---
+++ NewsFeedCreation/RSSFeedCreation.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.XPath;
+using System.Xml;
+using System.Xml.Xsl;
+
+namespace NewsFeedCreation
+{
+    [TestClass]
+    public class RSSFeedCreation
+    {
+        [TestMethod]
+        public void CheckRssFeedCreation()
+        {
+            var xsl = new XslCompiledTransform();
+            xsl.Load("RSSCreator.xslt");
+            var xslParams = new XsltArgumentList();
+            xslParams.AddParam("Date", "", DateTime.Now);
+
+
+            xsl.Transform("Content//Books.xml", xslParams, Console.Out);
+        }
+    }
+}
