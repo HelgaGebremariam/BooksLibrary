fafb78e82a8669ca371a3266825d273fd084db66
helga <Helga.gebremariam@gmail.com>
2017-04-08 20:53:54 +0300

    Convert data into correct format.

--- NewsFeedCreation/RSSCreator.xslt
+++ NewsFeedCreation/RSSCreator.xslt
@@ -7,11 +7,7 @@
     <msxsl:script language="JScript" implements-prefix="date">
        function today()
        {
-          var today = new Date();
-          var dd = today.getDate();
-          var mm = today.getMonth()+1; //January is 0!
-          var yyyy = today.getFullYear();
-          return yyyy+ "-" + mm + "-" + dd;
+          return new Date() + '';
        } 
     </msxsl:script> 
   
--- NewsFeedCreation/RSSFeedCreation.cs
+++ NewsFeedCreation/RSSFeedCreation.cs
@@ -1,11 +1,5 @@
 ﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Xml.XPath;
-using System.Xml;
 using System.Xml.Xsl;
 
 namespace NewsFeedCreation
@@ -20,8 +14,6 @@ namespace NewsFeedCreation
             xsl.Load("RSSCreator.xslt");
             var xslParams = new XsltArgumentList();
             xslParams.AddParam("Date", "", DateTime.Now);
-
-
             xsl.Transform("Content//Books.xml", xslParams, Console.Out);
         }
     }
